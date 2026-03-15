16a037f390d7437d155b8438453026ea30e6ed9f
Shashwat Raj <rajayshashwat@gmail.com>
2026-03-15 00:15:15 -0700

    clean

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -11,7 +11,7 @@ export function getFooterHTML(): string {
           <span class="footer-separator">•</span>
           <span class="footer-text">Inspired by <a href="https://www.nytimes.com/games/wordle/index.html" target="_blank" rel="noopener noreferrer">New York Times Wordle</a></span>
           <span class="footer-separator">•</span>
-          <span class="footer-text">&copy; ${new Date().getFullYear()} Numericle, All Rights Reserved</span>
+          <span class="footer-text">&copy; ${new Date().getFullYear()} <a href="https://www.github.com/darthvader58/numericle" target="_blank" rel="noopener noreferrer">Numericle</a>, All Rights Reserved</span>
         </div>
       </div>
     </footer>
